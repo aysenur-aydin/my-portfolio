4576bb52efc408bf80979549b338f65b448d8995
Aysenur Aydın <aysenuraydn08@gmail.com>
2023-09-02 17:44:26 +0300

    chore: Complete about section

--- components/intro.tsx
+++ components/intro.tsx
@@ -48,7 +48,7 @@ export default function Intro() {
         </div>
       </div>
 
-      <motion.p
+      <motion.h1
         className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
@@ -57,7 +57,7 @@ export default function Intro() {
         <span className="font-bold">front-end developer</span> with an{' '}
         <span className="font-bold">engineering background</span>. My focus is{' '}
         <span className="underline">React (Next.js)</span>
-      </motion.p>
+      </motion.h1>
 
       <motion.div
         className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
