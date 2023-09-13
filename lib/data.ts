2122024ee643e3183e6bac58faa320f5f4f377c3
Aysenur Aydın <aysenuraydn08@gmail.com>
2023-09-13 17:48:31 +0300

    docks: Add new project data

--- lib/data.ts
+++ lib/data.ts
@@ -1,6 +1,7 @@
 import React from 'react';
 import { CgWorkAlt } from 'react-icons/cg';
 import { LuGraduationCap } from 'react-icons/lu';
+import myPortfolio from '@/public/my-portfolio-project.png';
 import dashboardPageImg from '@/public/pendc_dashboard.png';
 import movieWatchlistImg from '@/public/movie-watchlist.png';
 import colorSchemeGeneratorImg from '@/public/color-scheme-generator.png';
@@ -62,9 +63,28 @@ export const experiencesData = [
 
 export const projectsData = [
   {
+    title: 'My Portfolio',
+    description: 'I build my portfolio website.',
+    tags: [
+      'Next.js',
+      'TypeScript',
+      'React',
+      'TailwindCss',
+      'react-dom',
+      'react-hot-toast',
+      'react-icons',
+      'react-email & resend',
+      'clsx',
+      'framer-motion',
+      'context API',
+    ],
+    imageUrl: myPortfolio,
+    projectUrl: 'https://aysenuraydin-portfolio.vercel.app/',
+  },
+  {
     title: 'PenDC Task',
     description:
-      'I completed a task for a job interview that required me to code a login and dashboard page adhering to Figma design.',
+      'I completed a task for PenDC that required me to code a login and dashboard page adhering to Figma design.',
     tags: [
       'React',
       'react-dom',
@@ -76,20 +96,20 @@ export const projectsData = [
     projectUrl: 'https://pendc-task-six.vercel.app/',
   },
   {
-    title: 'Color Scheme Generator',
-    description:
-      'This page allows users to create color schemes by selecting a starting color, and generates complementary, analogous, triadic, and other schemes.',
-    tags: ['API', 'JavaScript', 'HTML', 'CSS'],
-    imageUrl: colorSchemeGeneratorImg,
-    projectUrl: 'https://aysenur-aydin.github.io/Color-Scheme-Generator/',
-  },
-  {
     title: 'Movie Watchlist',
     description:
       'I developed a web page that enables users to search for movies and TV series by name and add them to a watchlist. This project is a part of the Scrimba Front-End Developer course.',
     tags: ['API', 'JavaScript', 'HTML', 'CSS'],
     imageUrl: movieWatchlistImg,
     projectUrl: 'https://aysenur-aydin.github.io/Movie-Watchlist/',
+  },
+  {
+    title: 'Color Scheme Generator',
+    description:
+      'This page allows users to create color schemes by selecting a starting color, and generates complementary, analogous, triadic, and other schemes.',
+    tags: ['API', 'JavaScript', 'HTML', 'CSS'],
+    imageUrl: colorSchemeGeneratorImg,
+    projectUrl: 'https://aysenur-aydin.github.io/Color-Scheme-Generator/',
   },
 ] as const;
 
